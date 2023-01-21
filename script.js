4457016add6f8d3c8a25d1af0a94c7a47e8a60ef
Chris Schubert <chris.schubert1994@gmail.com>
2023-01-21 18:02:03 -0500

    sorting the array of ride based on wait times

--- script.js
+++ script.js
@@ -4,36 +4,40 @@ const attractionEl = document.getElementById("attractions");
 const body = document.body;
 let ridesArray = [];
 
-
 async function getApi(url) {
   const response = await fetch(url);
   let data = await response.json();
   let attractions = data.liveData;
-  console.log(attractions);
-    Object.keys(attractions).forEach((key) => {
-        if (attractions[key].queue === undefined) {
-          return;
-        } else if(attractions[key].queue['STANDBY'] === undefined){
-            return;
-        } else if(attractions[key].queue['STANDBY'].waitTime === null){
-            return;
-        } else {
-          //console.log(`${attractions[key].name} : ${attractions[key].queue['STANDBY'].waitTime}`);
-          const para = document.createElement("p") ;
+  Object.keys(attractions).forEach((key) => {
+    if (attractions[key].queue === undefined) {
+      return;
+    } else if (attractions[key].queue["STANDBY"] === undefined) {
+      return;
+    } else if (attractions[key].queue["STANDBY"].waitTime === null) {
+      return;
+    } else {
+      //console.log(`${attractions[key].name} : ${attractions[key].queue['STANDBY'].waitTime}`);
 
-          
-
-
-          const node = document.createTextNode(`${attractions[key].name} : ${attractions[key].queue['STANDBY'].waitTime}` )
-          const child = document.getElementById('ride');
-          para.appendChild(node);
-          
-          attractionEl.appendChild(para, child);
-        }
-      });
-      
+      ridesArray.push(attractions[key]);
+    }
+  });
 }
 
 getApi(destinationsURL);
 
+ridesArray.sort((a, b) => {
+  return b.queue["STANDBY"].waitTime - a.queue["STANDBY"].waitTime;
+});
 
+function displayRides(array) {
+  array.forEach((key) => {
+      const para = document.createElement("p") ;
+      const node = document.createTextNode(`${array[key].name} : ${array[key].queue['STANDBY'].waitTime}` )
+      const child = document.getElementById('ride');
+      para.append(node);
+      attractionEl.append(para, child);
+  });
+}
+
+displayRides(ridesArray);
+console.log(ridesArray);

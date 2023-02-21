38c40acfb0960c3167a5f630ccf6a4b5df26c324
Chris Schubert <chris.schubert1994@gmail.com>
2023-02-20 19:33:58 -0500

    refactored JS code and added css animation for rides with wait times 30%+ under forecast

--- ridesAPI.js
+++ ridesAPI.js
@@ -11,7 +11,6 @@ let cYear = currentDate.getFullYear();
 //initializing empty array for the rides to be added to
 let ridesArray = [];
 let ridesForecast = [];
-let ridesArrayClone = [];
 let MKResult = 0;
 let epcotResult = 0;
 let AKResult = 0;
@@ -41,82 +40,14 @@ async function getApi(url) {
       return;
     } else if (attractions[key].queue["STANDBY"].waitTime === null) {
       return;
-    } else {
-      //adds attraction to array
+    } else if (attractions[key].forecast) {
       ridesArray.push(attractions[key]);
-
-      if (attractions[key].forecast) {
-        ridesArrayClone.push(attractions[key]);
-      }
 
       //sorts array in reverse so attractions with the longest
       //standby wait time are at the top
       ridesArray.sort((a, b) => {
         return b.queue["STANDBY"].waitTime - a.queue["STANDBY"].waitTime;
       });
-      ridesArrayClone.sort((a, b) => {
-        return b.queue["STANDBY"].waitTime - a.queue["STANDBY"].waitTime;
-      });
-    }
-  });
-
-  //iterates through list of attractions and displays each to
-  //the DOM while being sorted to their correct theme park
-  ridesArray.forEach((key) => {
-    //initializes dom element
-    let child;
-
-    //creates the p tag for the data to enter into
-    const para = document.createElement("p");
-
-    //generates the name of the attraction along with the
-    //current wait time for it
-    const node = document.createTextNode(
-      `${key.name} : ${key.queue["STANDBY"].waitTime} minutes`
-    );
-
-    //checks if attraction is in magic kingdom
-    if (key.parkId == "75ea578a-adc8-4116-a54d-dccb60765ef9") {
-      child = document.getElementById("Magic-Kingdom");
-
-      //appends the attraction and wait time into p tag
-      para.appendChild(node);
-
-      //appends the p tag into the correct div
-      child.appendChild(para, child);
-    }
-
-    //checks if attraction is in epcot
-    if (key.parkId == "47f90d2c-e191-4239-a466-5892ef59a88b") {
-      child = document.getElementById("Epcot");
-
-      //appends the attraction and wait time into p tag
-      para.appendChild(node);
-
-      //appends the p tag into the correct div
-      child.appendChild(para, child);
-    }
-
-    //checks if attraction is in animal kingdom
-    if (key.parkId == "1c84a229-8862-4648-9c71-378ddd2c7693") {
-      child = document.getElementById("Animal-Kingdom");
-
-      //appends the attraction and wait time into p tag
-      para.appendChild(node);
-
-      //appends the p tag into the correct div
-      child.appendChild(para, child);
-    }
-
-    //checks if attraction is in hollywood studios
-    if (key.parkId == "288747d1-8b4f-4a64-867e-ea7c9b27bad8") {
-      child = document.getElementById("Hollywood-Studios");
-
-      //appends the attraction and wait time into p tag
-      para.appendChild(node);
-
-      //appends the p tag into the correct div
-      child.appendChild(para, child);
     }
   });
 
@@ -144,6 +75,76 @@ async function getApi(url) {
     return difference;
   }
 
+  //iterates through list of attractions and displays each to
+  //the DOM while being sorted to their correct theme park
+  for (let i = 0; i < ridesArray.length; i++) {
+    //initializes dom element
+    let child;
+
+    //creates the p tag for the data to enter into
+    const para = document.createElement("p");
+    para.classList.add("ride");
+
+    if (
+      waitPercentDifference(
+        ridesArray[i].queue["STANDBY"].waitTime,
+        ridesForecast[i].FWT
+      ) < -0.3
+    ) {
+      para.classList.toggle("shimmer", true);
+    }
+
+    //generates the name of the attraction along with the
+    //current wait time for it
+    const node = document.createTextNode(
+      `${ridesArray[i].name} : ${ridesArray[i].queue["STANDBY"].waitTime} minutes`
+    );
+
+    //checks if attraction is in magic kingdom
+    if (ridesArray[i].parkId == "75ea578a-adc8-4116-a54d-dccb60765ef9") {
+      child = document.getElementById("Magic-Kingdom");
+
+      //appends the attraction and wait time into p tag
+      para.appendChild(node);
+
+      //appends the p tag into the correct div
+      child.appendChild(para, child);
+    }
+
+    //checks if attraction is in epcot
+    if (ridesArray[i].parkId == "47f90d2c-e191-4239-a466-5892ef59a88b") {
+      child = document.getElementById("Epcot");
+
+      //appends the attraction and wait time into p tag
+      para.appendChild(node);
+
+      //appends the p tag into the correct div
+      child.appendChild(para, child);
+    }
+
+    //checks if attraction is in animal kingdom
+    if (ridesArray[i].parkId == "1c84a229-8862-4648-9c71-378ddd2c7693") {
+      child = document.getElementById("Animal-Kingdom");
+
+      //appends the attraction and wait time into p tag
+      para.appendChild(node);
+
+      //appends the p tag into the correct div
+      child.appendChild(para, child);
+    }
+
+    //checks if attraction is in hollywood studios
+    if (ridesArray[i].parkId == "288747d1-8b4f-4a64-867e-ea7c9b27bad8") {
+      child = document.getElementById("Hollywood-Studios");
+
+      //appends the attraction and wait time into p tag
+      para.appendChild(node);
+
+      //appends the p tag into the correct div
+      child.appendChild(para, child);
+    }
+  }
+
   //Iterates through array, identifies which park the ride goes to,
   //Runs waitPercentDifference function, and updates the result for that park
   for (let i = 0; i < ridesForecast.length; i++) {
@@ -151,7 +152,7 @@ async function getApi(url) {
       MKResult =
         MKResult +
         waitPercentDifference(
-          ridesArrayClone[i].queue["STANDBY"].waitTime,
+          ridesArray[i].queue["STANDBY"].waitTime,
           ridesForecast[i].FWT
         );
     }
@@ -159,7 +160,7 @@ async function getApi(url) {
       epcotResult =
         epcotResult +
         waitPercentDifference(
-          ridesArrayClone[i].queue["STANDBY"].waitTime,
+          ridesArray[i].queue["STANDBY"].waitTime,
           ridesForecast[i].FWT
         );
     }
@@ -167,7 +168,7 @@ async function getApi(url) {
       AKResult =
         AKResult +
         waitPercentDifference(
-          ridesArrayClone[i].queue["STANDBY"].waitTime,
+          ridesArray[i].queue["STANDBY"].waitTime,
           ridesForecast[i].FWT
         );
     }
@@ -175,7 +176,7 @@ async function getApi(url) {
       HSResult =
         HSResult +
         waitPercentDifference(
-          ridesArrayClone[i].queue["STANDBY"].waitTime,
+          ridesArray[i].queue["STANDBY"].waitTime,
           ridesForecast[i].FWT
         );
     }
@@ -183,84 +184,120 @@ async function getApi(url) {
 
   //Initializes array with results and sorts them in ascending order
   let parksArray = [
-    { name: 'MagicKingdom', result: MKResult },
-    { name: 'Epcot', result: epcotResult },
-    { name: 'AnimalKingdom', result: AKResult },
-    { name: 'HollywoodStudios', result: HSResult },
+    { name: "MagicKingdom", result: MKResult },
+    { name: "Epcot", result: epcotResult },
+    { name: "AnimalKingdom", result: AKResult },
+    { name: "HollywoodStudios", result: HSResult },
   ];
   parksArray.sort((a, b) => {
     return a.result - b.result;
   });
 
-  for(let i=0; i < parksArray.length; i++){
+  for (let i = 0; i < parksArray.length; i++) {
     let title;
     //If the index is 0
-    if (parksArray[i].name == "MagicKingdom" && parksArray.findIndex(x => x.name == 'MagicKingdom') === 0){
-      title = document.getElementById('MK-title') 
-      title.append('🥇') 
-      console.log('magic kingdom is 1st')
+    if (
+      parksArray[i].name == "MagicKingdom" &&
+      parksArray.findIndex((x) => x.name == "MagicKingdom") === 0
+    ) {
+      title = document.getElementById("MK-title");
+      title.append("🥇");
+      console.log("magic kingdom is 1st");
     }
-    if (parksArray[i].name == "Epcot" && parksArray.findIndex(x => x.name == 'Epcot') === 0){
-      title = document.getElementById('epcot-title')
-      title.append('🥇')
-      console.log('epcot is 1st')
+    if (
+      parksArray[i].name == "Epcot" &&
+      parksArray.findIndex((x) => x.name == "Epcot") === 0
+    ) {
+      title = document.getElementById("epcot-title");
+      title.append("🥇");
+      console.log("epcot is 1st");
     }
-    if (parksArray[i].name == "AnimalKingdom" && parksArray.findIndex(x => x.name == 'AnimalKingdom') === 0){
-      title = document.getElementById('AK-title')
-      title.append('🥇')
-      console.log('animal kingdom is 1st')
+    if (
+      parksArray[i].name == "AnimalKingdom" &&
+      parksArray.findIndex((x) => x.name == "AnimalKingdom") === 0
+    ) {
+      title = document.getElementById("AK-title");
+      title.append("🥇");
+      console.log("animal kingdom is 1st");
     }
-    if (parksArray[i].name == "HollywoodStudios" && parksArray.findIndex(x => x.name == 'HollywoodStudios') === 0){
-      title = document.getElementById('HS-title')
-      title.append('🥇')
-      console.log('hollywood studios is 1st')
+    if (
+      parksArray[i].name == "HollywoodStudios" &&
+      parksArray.findIndex((x) => x.name == "HollywoodStudios") === 0
+    ) {
+      title = document.getElementById("HS-title");
+      title.append("🥇");
+      console.log("hollywood studios is 1st");
     }
 
     //If the index is 1
-    if (parksArray[i].name == "MagicKingdom" && parksArray.findIndex(x => x.name == 'MagicKingdom') === 1){
-      title = document.getElementById('MK-title')
-      title.append('🥈') 
-      console.log('magic kingdom is 2nd') 
+    if (
+      parksArray[i].name == "MagicKingdom" &&
+      parksArray.findIndex((x) => x.name == "MagicKingdom") === 1
+    ) {
+      title = document.getElementById("MK-title");
+      title.append("🥈");
+      console.log("magic kingdom is 2nd");
     }
-    if (parksArray[i].name == "Epcot" && parksArray.findIndex(x => x.name == 'Epcot') === 1){
-      title = document.getElementById('epcot-title')
-      title.append('🥈')
-      console.log('epcot is 2nd')
+    if (
+      parksArray[i].name == "Epcot" &&
+      parksArray.findIndex((x) => x.name == "Epcot") === 1
+    ) {
+      title = document.getElementById("epcot-title");
+      title.append("🥈");
+      console.log("epcot is 2nd");
     }
-    if (parksArray[i].name == "AnimalKingdom" && parksArray.findIndex(x => x.name == 'AnimalKingdom') === 1){
-      title = document.getElementById('AK-title')
-      title.append('🥈')
-      console.log('animal kingdom is 2nd')
+    if (
+      parksArray[i].name == "AnimalKingdom" &&
+      parksArray.findIndex((x) => x.name == "AnimalKingdom") === 1
+    ) {
+      title = document.getElementById("AK-title");
+      title.append("🥈");
+      console.log("animal kingdom is 2nd");
     }
-    if (parksArray[i].name == "HollywoodStudios" && parksArray.findIndex(x => x.name == 'HollywoodStudios') === 1){
-      title = document.getElementById('HS-title')
-      title.append('🥈')
-      console.log('hollywood studios is 2nd')
+    if (
+      parksArray[i].name == "HollywoodStudios" &&
+      parksArray.findIndex((x) => x.name == "HollywoodStudios") === 1
+    ) {
+      title = document.getElementById("HS-title");
+      title.append("🥈");
+      console.log("hollywood studios is 2nd");
     }
     //If the index is 2
-    if (parksArray[i].name == "MagicKingdom" && parksArray.findIndex(x => x.name == 'MagicKingdom') === 2){
-      title = document.getElementById('MK-title')
-      title.append('🥉')
-      console.log('magic kingdom is 3rd')  
+    if (
+      parksArray[i].name == "MagicKingdom" &&
+      parksArray.findIndex((x) => x.name == "MagicKingdom") === 2
+    ) {
+      title = document.getElementById("MK-title");
+      title.append("🥉");
+      console.log("magic kingdom is 3rd");
     }
-    if (parksArray[i].name == "Epcot" && parksArray.findIndex(x => x.name == 'Epcot') === 2){
-      title = document.getElementById('epcot-title')
-      title.append('🥉')
-      console.log('epcot is 3rd')
+    if (
+      parksArray[i].name == "Epcot" &&
+      parksArray.findIndex((x) => x.name == "Epcot") === 2
+    ) {
+      title = document.getElementById("epcot-title");
+      title.append("🥉");
+      console.log("epcot is 3rd");
     }
-    if (parksArray[i].name == "AnimalKingdom" && parksArray.findIndex(x => x.name == 'AnimalKingdom') === 2){
-      title = document.getElementById('AK-title')
-      title.append('🥉')
-      console.log('animal kingdom is 3rd')
+    if (
+      parksArray[i].name == "AnimalKingdom" &&
+      parksArray.findIndex((x) => x.name == "AnimalKingdom") === 2
+    ) {
+      title = document.getElementById("AK-title");
+      title.append("🥉");
+      console.log("animal kingdom is 3rd");
     }
-    if (parksArray[i].name == "HollywoodStudios" && parksArray.findIndex(x => x.name == 'HollywoodStudios') === 2){
-      title = document.getElementById('HS-title')
-      title.append('🥉')
-      console.log('hollywood studios is 3rd')
+    if (
+      parksArray[i].name == "HollywoodStudios" &&
+      parksArray.findIndex((x) => x.name == "HollywoodStudios") === 2
+    ) {
+      title = document.getElementById("HS-title");
+      title.append("🥉");
+      console.log("hollywood studios is 3rd");
     }
   }
-
-
+  console.log(ridesArray);
+  console.log(ridesForecast);
   console.log(parksArray);
 }
 
